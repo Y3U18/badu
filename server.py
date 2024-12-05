829741495c79e1da2b0c709f310f904bddda0302
hodor <hodor@gmail.com>
2024-12-05 17:20:41 +0800

    refactor.

--- server.py
+++ server.py
@@ -6,9 +6,15 @@ from urllib.parse import urlparse, parse_qs
 import requests
 import os
 import re
-
+from concurrent.futures import ThreadPoolExecutor
 import logging
+import threading
+import queue
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# 创建一个队列
+task_queue = queue.Queue()
 
 def fetch_video_detail(item_id, language='zh-CN',  retry=5):
     url = "https://www.tiktok.com/player/api/v1/items"
@@ -44,7 +50,7 @@ def fetch_video_detail(item_id, language='zh-CN',  retry=5):
     return None
 
     
-def download_file(url, filename, retry=5):
+def download_file(url, filename, retry=5):   
     dir_path = os.path.dirname(filename)
     if  not os.path.exists(dir_path):
         os.makedirs(dir_path)
@@ -57,7 +63,7 @@ def download_file(url, filename, retry=5):
                     file.write(response.content)
                 return True
         except Exception as e:
-            logging.error(f"download failed err: {e} url {url}")
+            logging.error(f"download failed err: {e}")
     return False
 
 def fetch_videos(product_id, page):
@@ -79,40 +85,38 @@ def fetch_videos(product_id, page):
             'utm_south': 'google',
             'utm_id': 'ggproduct',
             '_src': '',
-            'fp_visid': '452a4dbde35177302ee92f199b5b18dc',
-            'fd_id': 'sH4tmUr5A7gpiW8yRe1qCnSwDGOocN6J',
-            '_fbp': 'fb.1.1732692190061.123470053690447508',
-            '_ga': 'GA1.1.1177071262.1732692199',
+            'fp_visid': '68e07a174825b8bc0ee0769ef6ee03fc',
+            'fd_id': 'UrfKhQSzs6eI415nBOjF8TkCMladVAqP',
+            'Hm_lvt_6ada669245fc6950ae4a2c0a86931766': '1732585434',
+            'HMACCOUNT': 'DE784806A161133E',
+            '_fbp': 'fb.1.1732585434257.605751515129595828',
+            '_ga': 'GA1.1.468739000.1732585453',
             '_tt_enable_cookie': '1',
-            '_ttp': 'mNn9Fu0EZ072X7gphbWsyaMUg4n.tt.1',
-            '_ss_s_uid': 'cea9581a6d29086e49cb688dbc0de6af',
+            '_ttp': 'bmZ0vYQWnabrDogGvJTDTpl5yFa.tt.1',
             'utm_lang': 'zh',
-            'Hm_lvt_6ada669245fc6950ae4a2c0a86931766': '1732692180,1732754778',
-            'HMACCOUNT': '25778556149293FF',
-            '_gcl_aw': 'GCL.1732841234.CjwKCAiAl4a6BhBqEiwAqvrqutdN-l76yWm3WXzSjKe8yN5-2e3csWm4GsvvO-jQ_CK17mHc5HCcgBoCjLYQAvD_BwE',
-            'Hm_lpvt_6ada669245fc6950ae4a2c0a86931766': '1733274355',
-            '_gcl_au': '1.1.109408670.1732692199.1378703260.1733280994.1733280994',
-            'fd_tk_exp': '1734577124',
-            'fd_tk': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzQ1NzcxMjQsInN1YiI6IjJlNTc0OTZiMDIxY2U5YWUxNzY0ZjE3OTBiY2NiNDQwIiwibmJmIjoxNzMzMjgxMTI0LCJhdWQiOnsidWlkIjo5MzY5NDgwLCJ1bmlvbmlkIjoiIiwibWNfb3BlbmlkIjoiIiwibmlja25hbWUiOiJGYXN0TW9zc-eUqOaItyIsInJlZ2lvbiI6IlVTIiwiY3JlYXRlZF9hdCI6MTczMzI4MTEyMywiY3JlYXRlZF9kYXRlIjoiMjAyNC0xMi0wNCIsImxvZ2luX3NvdXJjZSI6InBjIiwidmlzaXRvcl9pZCI6IjQ1MmE0ZGJkZTM1MTc3MzAyZWU5MmYxOTliNWIxOGRjIiwiaXAiOiI1LjM0LjIxNi4xMTgiLCJkb21haW4iOiJ3d3cuZmFzdG1vc3MuY29tIiwiZnBfdmlzaWQiOiJiODc2ZmUzN2E1MmU2ZDk1MjllMGViYTQ4ODA1NmExMiIsImNyZWF0ZV90aW1lIjoxNzMzMjgxMTI0fSwiaWF0IjoxNzMzMjgxMTI0LCJqdGkiOiIyZTU3NDk2YjAyMWNlOWFlMTc2NGYxNzkwYmNjYjQ0MCIsImlzcyI6Ind3dy5mYXN0bW9zcy5jb20iLCJzdGF0dXMiOjEsImRhdGEiOm51bGx9.swFc7YqNrrXMNDycKcakCo1Ayn0m78mt73CRYruH30M',
-            'free_trial': 'FreeTrialFalse',
+            '_ss_s_uid': '8ea7c8961ea34162544ea58a6f611740',
+            '_gcl_aw': 'GCL.1733106169.CjwKCAiAl4a6BhBqEiwAqvrqutdN-l76yWm3WXzSjKe8yN5-2e3csWm4GsvvO-jQ_CK17mHc5HCcgBoCjLYQAvD_BwE',
+            '_gcl_au': '1.1.1894929420.1732585453.17552096.1733279143.1733279143',
+            'fd_tk_exp': '1734575248',
+            'fd_tk': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzQ1NzUyNDgsInN1YiI6IjI3YmJhN2MwMTYwZmI4ZjIwZDhjNmFmYTkyN2RlMGFkIiwibmJmIjoxNzMzMjc5MjQ4LCJhdWQiOnsidWlkIjo5MzY3NzIyLCJ1bmlvbmlkIjoiIiwibWNfb3BlbmlkIjoiIiwibmlja25hbWUiOiJGYXN0TW9zc-eUqOaItyIsInJlZ2lvbiI6IlVTIiwiY3JlYXRlZF9hdCI6MTczMzI3OTI0NywiY3JlYXRlZF9kYXRlIjoiMjAyNC0xMi0wNCIsImxvZ2luX3NvdXJjZSI6InBjIiwidmlzaXRvcl9pZCI6IjY4ZTA3YTE3NDgyNWI4YmMwZWUwNzY5ZWY2ZWUwM2ZjIiwiaXAiOiIxNjMuMTI1LjE5My4yNiIsImRvbWFpbiI6Ind3dy5mYXN0bW9zcy5jb20iLCJmcF92aXNpZCI6Ijk0MjhiZDM1OTZhNjdmZTk1MmJmNjNiZDAxOTg1NDAyIiwiY3JlYXRlX3RpbWUiOjE3MzMyNzkyNDh9LCJpYXQiOjE3MzMyNzkyNDgsImp0aSI6IjI3YmJhN2MwMTYwZmI4ZjIwZDhjNmFmYTkyN2RlMGFkIiwiaXNzIjoid3d3LmZhc3Rtb3NzLmNvbSIsInN0YXR1cyI6MSwiZGF0YSI6bnVsbH0.Me2RuVMdUHjWnsKygAjsgV6auqB9qB4ocoUcRCOmNms',
             'NEXT_LOCALE': 'zh',
             'region': 'Global',
-            '_ga_J8P3E5KDGJ': 'GS1.1.1733280990.6.1.1733281214.60.0.1625003845',
-            '_ga_GD8ST04HB5': 'GS1.1.1733280990.6.1.1733281214.60.0.244846493',
-            '__stripe_mid': 'cc75a684-38d8-4efd-aca1-7d2e41959cd7484817',
-            '__stripe_sid': '27771cfe-f894-4032-8651-705dc81e3173bd55c4',
-            '_uetsid': '31d2edc0b05411ef859059c474939c77|d0gbhu|2|frf|0|1797',
-            '_uetvid': '6ec88190ac9011ef9f65177761bd263a|yoa2vk|1733281245994|13|1|bat.bing.com/p/insights/c/k',
+            '_ga_J8P3E5KDGJ': 'GS1.1.1733279135.10.1.1733279270.34.0.1197439520',
+            '_ga_GD8ST04HB5': 'GS1.1.1733279135.10.1.1733279270.34.0.411894322',
+            'Hm_lpvt_6ada669245fc6950ae4a2c0a86931766': '1733327778',
+            '_uetsid': 'e5a09f30ab9711ef965fe55fae453704|jksuoi|2|frf|0|1791',
+            '_uetvid': 'e5a098e0ab9711efb467d595a47500a2|gik53o|1733327778576|70|1|bat.bing.com/p/insights/c/k',
         }
+
 
         headers = {
             'accept': 'application/json, text/plain, */*',
             'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-            # 'cookie': 'utm_south=google; utm_id=ggproduct; _src=; fp_visid=452a4dbde35177302ee92f199b5b18dc; fd_id=sH4tmUr5A7gpiW8yRe1qCnSwDGOocN6J; _fbp=fb.1.1732692190061.123470053690447508; _ga=GA1.1.1177071262.1732692199; _tt_enable_cookie=1; _ttp=mNn9Fu0EZ072X7gphbWsyaMUg4n.tt.1; _ss_s_uid=cea9581a6d29086e49cb688dbc0de6af; utm_lang=zh; Hm_lvt_6ada669245fc6950ae4a2c0a86931766=1732692180,1732754778; HMACCOUNT=25778556149293FF; _gcl_aw=GCL.1732841234.CjwKCAiAl4a6BhBqEiwAqvrqutdN-l76yWm3WXzSjKe8yN5-2e3csWm4GsvvO-jQ_CK17mHc5HCcgBoCjLYQAvD_BwE; Hm_lpvt_6ada669245fc6950ae4a2c0a86931766=1733274355; _gcl_au=1.1.109408670.1732692199.1378703260.1733280994.1733280994; fd_tk_exp=1734577124; fd_tk=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzQ1NzcxMjQsInN1YiI6IjJlNTc0OTZiMDIxY2U5YWUxNzY0ZjE3OTBiY2NiNDQwIiwibmJmIjoxNzMzMjgxMTI0LCJhdWQiOnsidWlkIjo5MzY5NDgwLCJ1bmlvbmlkIjoiIiwibWNfb3BlbmlkIjoiIiwibmlja25hbWUiOiJGYXN0TW9zc-eUqOaItyIsInJlZ2lvbiI6IlVTIiwiY3JlYXRlZF9hdCI6MTczMzI4MTEyMywiY3JlYXRlZF9kYXRlIjoiMjAyNC0xMi0wNCIsImxvZ2luX3NvdXJjZSI6InBjIiwidmlzaXRvcl9pZCI6IjQ1MmE0ZGJkZTM1MTc3MzAyZWU5MmYxOTliNWIxOGRjIiwiaXAiOiI1LjM0LjIxNi4xMTgiLCJkb21haW4iOiJ3d3cuZmFzdG1vc3MuY29tIiwiZnBfdmlzaWQiOiJiODc2ZmUzN2E1MmU2ZDk1MjllMGViYTQ4ODA1NmExMiIsImNyZWF0ZV90aW1lIjoxNzMzMjgxMTI0fSwiaWF0IjoxNzMzMjgxMTI0LCJqdGkiOiIyZTU3NDk2YjAyMWNlOWFlMTc2NGYxNzkwYmNjYjQ0MCIsImlzcyI6Ind3dy5mYXN0bW9zcy5jb20iLCJzdGF0dXMiOjEsImRhdGEiOm51bGx9.swFc7YqNrrXMNDycKcakCo1Ayn0m78mt73CRYruH30M; free_trial=FreeTrialFalse; NEXT_LOCALE=zh; region=Global; _ga_J8P3E5KDGJ=GS1.1.1733280990.6.1.1733281214.60.0.1625003845; _ga_GD8ST04HB5=GS1.1.1733280990.6.1.1733281214.60.0.244846493; __stripe_mid=cc75a684-38d8-4efd-aca1-7d2e41959cd7484817; __stripe_sid=27771cfe-f894-4032-8651-705dc81e3173bd55c4; _uetsid=31d2edc0b05411ef859059c474939c77|d0gbhu|2|frf|0|1797; _uetvid=6ec88190ac9011ef9f65177761bd263a|yoa2vk|1733281245994|13|1|bat.bing.com/p/insights/c/k',
-            'fm-sign': 'a78c099e14dbf13c13bcd38b3e8c0810',
+            # 'cookie': 'utm_south=google; utm_id=ggproduct; _src=; fp_visid=68e07a174825b8bc0ee0769ef6ee03fc; fd_id=UrfKhQSzs6eI415nBOjF8TkCMladVAqP; Hm_lvt_6ada669245fc6950ae4a2c0a86931766=1732585434; HMACCOUNT=DE784806A161133E; _fbp=fb.1.1732585434257.605751515129595828; _ga=GA1.1.468739000.1732585453; _tt_enable_cookie=1; _ttp=bmZ0vYQWnabrDogGvJTDTpl5yFa.tt.1; utm_lang=zh; _ss_s_uid=8ea7c8961ea34162544ea58a6f611740; _gcl_aw=GCL.1733106169.CjwKCAiAl4a6BhBqEiwAqvrqutdN-l76yWm3WXzSjKe8yN5-2e3csWm4GsvvO-jQ_CK17mHc5HCcgBoCjLYQAvD_BwE; _gcl_au=1.1.1894929420.1732585453.17552096.1733279143.1733279143; fd_tk_exp=1734575248; fd_tk=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzQ1NzUyNDgsInN1YiI6IjI3YmJhN2MwMTYwZmI4ZjIwZDhjNmFmYTkyN2RlMGFkIiwibmJmIjoxNzMzMjc5MjQ4LCJhdWQiOnsidWlkIjo5MzY3NzIyLCJ1bmlvbmlkIjoiIiwibWNfb3BlbmlkIjoiIiwibmlja25hbWUiOiJGYXN0TW9zc-eUqOaItyIsInJlZ2lvbiI6IlVTIiwiY3JlYXRlZF9hdCI6MTczMzI3OTI0NywiY3JlYXRlZF9kYXRlIjoiMjAyNC0xMi0wNCIsImxvZ2luX3NvdXJjZSI6InBjIiwidmlzaXRvcl9pZCI6IjY4ZTA3YTE3NDgyNWI4YmMwZWUwNzY5ZWY2ZWUwM2ZjIiwiaXAiOiIxNjMuMTI1LjE5My4yNiIsImRvbWFpbiI6Ind3dy5mYXN0bW9zcy5jb20iLCJmcF92aXNpZCI6Ijk0MjhiZDM1OTZhNjdmZTk1MmJmNjNiZDAxOTg1NDAyIiwiY3JlYXRlX3RpbWUiOjE3MzMyNzkyNDh9LCJpYXQiOjE3MzMyNzkyNDgsImp0aSI6IjI3YmJhN2MwMTYwZmI4ZjIwZDhjNmFmYTkyN2RlMGFkIiwiaXNzIjoid3d3LmZhc3Rtb3NzLmNvbSIsInN0YXR1cyI6MSwiZGF0YSI6bnVsbH0.Me2RuVMdUHjWnsKygAjsgV6auqB9qB4ocoUcRCOmNms; NEXT_LOCALE=zh; region=Global; _ga_J8P3E5KDGJ=GS1.1.1733279135.10.1.1733279270.34.0.1197439520; _ga_GD8ST04HB5=GS1.1.1733279135.10.1.1733279270.34.0.411894322; Hm_lpvt_6ada669245fc6950ae4a2c0a86931766=1733327778; _uetsid=e5a09f30ab9711ef965fe55fae453704|jksuoi|2|frf|0|1791; _uetvid=e5a098e0ab9711efb467d595a47500a2|gik53o|1733327778576|70|1|bat.bing.com/p/insights/c/k',
+            'fm-sign': '219fe10a9b2e42171c2aa651cd65b9af',
             'lang': 'ZH_CN',
             'priority': 'u=1, i',
-            'referer': 'https://www.fastmoss.com/zh/e-commerce/detail/1729829773915949142',
+            'referer': 'https://www.fastmoss.com/zh/e-commerce/detail/1729991365114368343',
             'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': '"macOS"',
@@ -135,53 +139,64 @@ def fetch_videos(product_id, page):
             return None
     
         res = response.json()
-        logging.info(f"res: {res}")
-        if res['code'] != 200:
-            logging.error(f"fetch_videos {res['msg']}")
-        return res['data']['list']
+        logging.debug(f"res: {res}")
+        return res
             
     except requests.RequestException as e:
         logging.error("请求过程中发生错误：", e)
         return None
 
-def download_videos(product_id):
-    videos = fetch_videos(product_id, 1)
-    res = []
-    for v in videos:
-        res.append(download_video(product_id, v['video_id']))
-    date = time.strftime("%Y%m%d", time.localtime())
-    logging.info(f"please go to http://120.79.221.205:6801/files/tk_videos/{date}/{product_id}/")
+def download_video_thread(product_id, video_id):
+    try:
+        result = download_video(product_id, video_id)
+        return result
+    except Exception as e:
+        logging.error(f"Failed to download video {video_id}: {e}")
+        return None
 
-    return {'link': f"http://120.79.221.205:6801/files/tk_videos/{date}/{product_id}/", 'detail':res}
+
+
+
 
 def download_video(product_id, video_id):
-    logging.info(f"downloading video_id:{video_id}")
-    result = {'ok':False, 'video_id':video_id}
+    logging.info(f"{product_id}:{video_id} begining download video")
     video_detail = fetch_video_detail(video_id)
     if video_detail['status_code'] != 0:
-        result["msg"] = f"fetch_tiktok_video failed {video_detail['status_msg']}"
-        return result
+        logging.warn(f"fetch_tiktok_video failed {video_detail['status_msg']}")
+        return
     try:
         vedio_url =  video_detail['items'][0]['video_info']['url_list'][0]
         desc = video_detail['items'][0]['desc']
-        
-        logging.debug(f"downloading  vedio_url:{vedio_url}")
         date = time.strftime("%Y%m%d", time.localtime())
-        if not download_file(vedio_url, f"tk_videos/{date}/{product_id}/{video_id}.mp4") :
-            result["msg"] = f"download_file failed url:{vedio_url}"
-            return result
+        filename = f"tk_videos/{date}/{product_id}/{video_id}.mp4"
+        
+        if  os.path.exists(filename):
+            logging.info(f"{product_id}:{video_id} already downloaded!")
+            return True
+    
+        if not download_file(vedio_url, filename) :
+            logging.info(f"{product_id}:{video_id}  already downloaded!")
+            return
         
         with open(f"tk_videos/{date}/{product_id}/text", 'a') as file:
             file.write(f'{desc}\n')  
-        result['ok']  = True
-        return result
+        logging.info(f"{product_id}:{video_id}  download video success")
     except Exception as e:
-        result["msg"] = f"fetch_tiktok_video failed {e}"
-        logging.error(f'download_video error: {e}')
-        return result
+        logging.error(f"{product_id}:{video_id} begining download video {e}")
+
+def consumer_thread(task_queue):
+    while True:
+        # 获取任务，如果队列为空，则等待
+        product_id, videos = task_queue.get()
+        if product_id is None:
+            break  # 如果接收到None，则退出循环
     
-    
-    
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            futures = [executor.submit(download_video_thread, product_id, v['video_id']) for v in videos]
+
+    task_queue.task_done()  # 标记任务为完成
+
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
@@ -193,18 +208,44 @@ def extract_product_id(url):
         return match.group()
     else:
         return None
-    
+
+@app.route('/get_task_status', methods=['GET'])
+def get_task_status():
+    # 将所有日志信息返回给前端
+    # log='\n'.join(log_messages)
+    # log_messages.clear()
+    #return log
+    pass
+
 @app.route('/download', methods=['POST'])
 def download():
     product_link = request.form['product_link'] 
-    #product_link = "https://www.fastmoss.com/zh/e-commerce/detail/1729488621914722962"
-    if product_link is None or len(product_link) == 0 :
+    if product_link is None or len(product_link) == 0 :        
         return jsonify({"ok": False, "error": "No product_link provided"}) 
-    
     product_id = extract_product_id(product_link)
     logging.info(f"product_link:{product_link} product_id:{product_id}")
-    data = download_videos(product_id)  
-    return jsonify({"ok": True, "data": data})
+    
+   
+    res = fetch_videos(product_id, 1)
+    if res['code'] != 200:
+        return jsonify({"ok": False, "error": res['msg']}) 
+    
+    task_queue.put((product_id, res['data']['list']))
+    date = time.strftime("%Y%m%d", time.localtime())    
+    
+    link = f"http://120.79.221.205:6801/files/tk_videos/{date}/{product_id}/"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5001, debug=False)
+    return jsonify({"ok": True, 'link': link, 'product_id': product_id})
+
+def app_run():
+    logging.info("Flask app is running")
+    app.run(app.run(host='0.0.0.0', port=5001, debug=False))
+
+flask_thread = threading.Thread(target=app_run)
+flask_thread.start()
+
+consumer = threading.Thread(target=consumer_thread, args=(task_queue,))
+consumer.start()
+
+flask_thread.join()
+consumer.join()
